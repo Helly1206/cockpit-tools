9c33f59e9f0e8da2302fa5d7327b88eb06cc2c2a
Helly <hellyrulez@home.nl>
2021-08-30 11:33:14 +0200

    v0.81

--- README.txt
+++ README.txt
@@ -1,18 +1,18 @@
-cockpit-tools v0.80
+cockpit-tools v0.81
 
 cockpit-tools -- Add links to external webtools to cockpit
 ============= == === ===== == ======== ======== == =======
 
-This plugin not heavily documented yet, but that is probably not nessessary. It's a cockit UI to show links to external tools in cockpit.
+This plugin not heavily documented yet, but that is probably not necessary. It's a cockpit UI to show links to external tools in cockpit.
 
 It uses cockpit-stdplgin as standard look and feel for this UI.
 
-Tools can be added/ deleted in /etc/cockpit-tools.xml or via /opt/tools-cli.py via commandline. see /opt/tools-cli.py -h for more info.
+Tools can be added/ deleted in /etc/cockpit-tools.xml or via /opt/tools/tools-cli.py via command line. see /opt/tools/tools-cli.py -h for more info.
 
 <toolname>
     <icon>/etc/whatever.png</icon>
     <ref>https://192.168.1.1/example</ref>
-</toolname>    
+</toolname>
 
 That's all for now ...
 
--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 NAME="tools"
 LNAME="cockpit-$NAME"
+CLINAME="$NAME-cli.py"
 USRDIR="/usr"
+OPTDIR="/opt"
 CKPTDIR="share/cockpit"
 USRLOC="$USRDIR/$CKPTDIR/$NAME"
+OPTLOC="$OPTDIR/$NAME"
 DEBFOLDER="debian"
+OPTCLI="$OPTLOC/$CLINAME"
 
 minify_install () {
     echo "Installing and compiling files"
-    
+
     which minify >/dev/null
     if [ $? -ne 0 ]; then
         echo "Minify not installed. Install minify first"
         echo "e.g. by: sudo apt install minify"
         exit
     fi
-    
+
+    if [ ! -d ".$USRLOC" ]; then
+        mkdir -p ".$USRLOC"
+    fi
+
     minify -r -o ".$USRLOC" --match="\.js" $NAME
     #minify -r -o ".$USRLOC" --match="\.css" $NAME
     #minify -r -o ".$USRLOC" --match="\.html" $NAME
@@ -38,6 +46,9 @@ then
 	if [ -d "$USRLOC" ]; then
         rm -rf "$USRLOC"
     fi
+    if [ -d "$OPTCLI" ]; then
+        rm -f "$OPTCLI"
+    fi
 
 elif [ "$1" == "-h" ] || [ "$1" == "-H" ]
 then
@@ -50,7 +61,7 @@ then
 elif [ "$1" == "-c" ] || [ "$1" == "-C" ]
 then
 	echo "$LNAME Deleting compiled files in install folder"
-	rm -f ".$USRLOC"/*
+	rm -rf ".$USRDIR"
 	rm -f ./*.deb
 	rm -rf "$DEBFOLDER"/$LNAME
 	rm -rf "$DEBFOLDER"/.debhelper
@@ -66,11 +77,25 @@ then
 else
 	echo "$LNAME install script"
     minify_install
-    
+
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python3-pil|grep "install ok installed")
+    echo Checking for python3-pil: $PKG_OK
+    if [ "" == "$PKG_OK" ]; then
+        echo "No python3-pil. Setting up python3-pil."
+        sudo apt-get --force-yes --yes install python3-pil
+    fi
+
     if [ ! -d "$USRLOC" ]; then
         mkdir "$USRLOC"
     fi
-    cp -r ".$USRLOC/." "$USRLOC/"
-	
-    
+    if [ -d ".$USRLOC" ]; then
+        cp -r ".$USRLOC/." "$USRLOC/"
+    fi
+
+    if [ ! -d "$OPTLOC" ]; then
+        mkdir "$OPTLOC"
+    fi
+    if [ -d ".$OPTLOC" ]; then
+        cp -r ".$OPTLOC/." "$OPTLOC/"
+    fi
 fi
